90be3d9ae6b5cbf0eacf5d7fec6693e9f4500fda
makkut <makkut@users.noreply.github.com>
2021-01-08 22:23:32 +0100

    Init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+const Header = () =>{
+    return(
+        <header className='header'>
+            <img src='https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_4.png'/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className='content-box'>
+                <img className='content-img'
+                     src='https://moscow-oblast.sm-news.ru/wp-content/uploads/2020/06/25/prekrasnye_pljazhi_praslina_sejshely.jpg'/>
+            </div>
+            <div className='content-text'>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        <div>
+                            Post1
+                        </div>
+                        <div>
+                            Post2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
